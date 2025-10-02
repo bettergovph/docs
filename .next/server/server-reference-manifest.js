6b7b5c9cb109796948d21fc5024857c8e1fccbe9
Justin322322 <justinmarlosibonga@gmail.com>
2025-10-02 13:27:26 +0800

    Restructure project: move docs to root and clean up build files

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"p2Qv+p+q8ki6ktl298ZH6ulzArEXGr0gpkC12OT0uKQ=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
--- .next/static/chunks/src_app_layout_tsx_0a548d63._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__0785f11c._.css",
-  "static/chunks/node_modules_fumadocs-ui_dist_components_dialog_search-default_7a58bdc7.js",
-  "static/chunks/node_modules_e8f920b0._.js"
-],
-    source: "dynamic"
-});
